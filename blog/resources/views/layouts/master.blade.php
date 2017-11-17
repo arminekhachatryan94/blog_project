2fbf12b3f870b2a0457584591847e98ffd2dc5c1
Armine Khachatryan <armine.khachatryan94@yahoo.com>
2017-11-17 11:25:20 -0800

    laravel mix & front-end

--- blog/resources/views/layouts/master.blade.php
+++ blog/resources/views/layouts/master.blade.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
 
-    <link rel="stylesheet" href="/css/styles.css">
+    <link rel="stylesheet" href="/css/app.css">
 
     <script type="text/javascript" src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
         <!--script type="text/javascript" src="jquery.js"></script-->
