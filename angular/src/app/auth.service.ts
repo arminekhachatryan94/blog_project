72a215885a997bde1cfb632f937b70433dde761a
Armine Khachatryan <armine.khachatryan94@yahoo.com>
2017-11-24 15:04:55 -0800

    login & store token

--- angular/src/app/auth.service.ts
+++ angular/src/app/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { Http, Headers, Response } from "@angular/http";
 import { Observable } from 'rxjs';
+import 'rxjs/Rx';
 
 @Injectable()
 export class AuthService {
@@ -12,6 +13,24 @@ export class AuthService {
         return this.http.post('http://127.0.0.1:8000/api/register',
             { name: name, email: email, password: password },
             { headers: new Headers({'X-Requested-With': 'XMLHttpRequest'}) }
+        );
+    }
+
+    login(email: string, password: string){
+        return this.http.post('http://127.0.0.1:8000/api/login',
+            { email: email, password: password },
+            { headers: new Headers({'X-Requested-With': 'XMLHttpRequest'}) }
+        ).map(
+            (response: Response) => {
+                const token = response.json().token;
+                const base64Url = token.split('.')[1];
+                const base64 = base64Url.replace('-', '+').replace('_', '/');
+                return {token: token, decoded: JSON.parse(window.atob(base64))};
+            }
+        ).do(
+            tokenData => {
+                localStorage.setItem('token', tokenData.token);
+            }
         );
     }
 }
